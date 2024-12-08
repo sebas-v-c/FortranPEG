b3236d046c164bb52e3f32178235a66d3a43d675
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2024-12-07 23:34:10 -0600

    probar todos los archivos al mismo tiempo

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from "vitest";
 import * as Parser from "../parser/fortranpeg";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 describe("Parser con Peggy", () => {
     // PRUEBA PREDETERMINADA PARA PRObAR
@@ -21,5 +23,29 @@ unarias
          `;
 
         const result = Parser.parse(Entrada);
+    });
+
+    it("Prueba para archivo de prueba 1", () => {
+        const input = readFileSync(join(__dirname, 'archivos_prueba', 'prueba1.txt'), 'utf-8')
+        const result = Parser.parse(input);
+        expect(result).toBeDefined();
+    });
+
+
+    it("Prueba para archivo de prueba 2", () => {
+        const input = readFileSync(join(__dirname, 'archivos_prueba', 'prueba2.txt'), 'utf-8')
+        const result = Parser.parse(input);
+        expect(result).toBeDefined();
+    });
+
+
+    it("Prueba para archivo de prueba 3", () => {
+        const input = readFileSync(join(__dirname, 'archivos_prueba', 'prueba3.txt'), 'utf-8')
+        const result = Parser.parse(input);
+        expect(result).toBeDefined();
+    });
+
+    it('Deberia fallar al tratar de validar entrada incorrecta', () => {
+        expect(() => Parser.parse("a = 'si'")).toThrow();
     });
 });
